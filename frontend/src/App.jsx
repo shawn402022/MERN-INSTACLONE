7be14b14dcc09b3dcbed7c676c6c431cf2cb959e
Shawn <sgiovanni1@yahoo.com>
2025-08-05 19:11:11 -0400

    installed react, tailwind, and shadcs

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <Button className="text-center text-4xl text-blue-500">Instagram Clone </Button>
+    </div>
+  )
+}
+
+export default App
