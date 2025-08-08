a08f88b9706c796d4769e11af78cdec7fabb6de2
Shawn <sgiovanni1@yahoo.com>
2025-08-08 12:59:02 -0400

    finished sign up page functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { Toaster } from 'sonner'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Toaster/>
   </StrictMode>,
 )
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,29 +2,114 @@ import React from 'react'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { toast } from 'sonner'
+import axios from 'axios'
 
 
 const Signup = () => {
+
+    // State to handle form input(username, email, password)
+    const [input, setInput] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    const [loading, setLoading] = useState(false)
+
+    //Function to handle input change
+    const changeEventHandler = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //Function to handle form submission
+    const signupHandler = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const res = await axios.post("http://localhost:8000/api/v1/user/register", input,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        withCredentials: true
+                    }
+                }
+            );
+
+            //Check to see if registration was successful or not
+            if (res.data.success) {
+
+                toast.success(res.data.message)
+                setInput({
+                    username: "",
+                    email: "",
+                    password: ""
+                })
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message)
+        } finally{
+            setLoading(false)
+        }
+
+    }
+
     return (
         <div className="flex items-center justify-center w-screen h-screen">
-            <form className="shadow-lg flex-col gap-5 p-8">
+            <form onSubmit={signupHandler} className="shadow-lg flex-col gap-5 p-8">
                 <div className="my-4 text-center">
                     <h1 className='text-xl font-bold'>Logo</h1>
                     <p className="text-sm">Sign up to see photos and videos from our friends </p>
                 </div>
                 <div>
-                    <Label htmlfor='Username' className='mb-1'>Username</Label>
-                    <Input type="text" name='username'></Input>
+                    <Label
+                        htmlfor='Username'
+                        className='mb-1'
+                    >Username
+                    </Label>
+                    <Input
+                        type="text"
+                        name='username'
+                        value={input.username}
+                        onChange={changeEventHandler}
+                    >
+                    </Input>
                 </div>
                 <div>
-                    <Label htmlfor='Email' className='mb-1'>Email</Label>
-                    <Input type="text" name='email'></Input>
+                    <Label
+                        htmlfor='Email'
+                        className='mb-1'
+                    >Email
+                    </Label>
+                    <Input
+                        type="email"
+                        name='email'
+                        value={input.email}
+                        onChange={changeEventHandler}
+                    >
+                    </Input>
                 </div>
                 <div>
-                    <Label htmlfor='Password' className='mb-1'>Password</Label>
-                    <Input type="password" name='password'></Input>
+                    <Label
+                        htmlfor='Password'
+                        className='mb-1'
+                    >Password
+                    </Label>
+                    <Input
+                        type="password"
+                        name='password'
+                        value={input.password}
+                        onChange={changeEventHandler}
+                    >
+                    </Input>
                 </div>
-                <Button type='submit' className='mb-1'>Sign Up</Button>
+                <Button type='submit' disable={loading}>{loading ? "Sign up..." : "Sign up"}</Button>
             </form>
 
 
