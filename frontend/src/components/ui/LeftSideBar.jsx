ba08fe3a4ee24dae68f6bc500967b3bcc0dae350
Shawn <sgiovanni1@yahoo.com>
2025-08-09 00:47:17 -0400

    created logout functionality

--- frontend/src/components/ui/LeftSideBar.jsx
+++ frontend/src/components/ui/LeftSideBar.jsx
@@ -1,15 +1,35 @@
 
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import axios from "axios"
 import { Heart, Home, LogOut, MessageCircle, PlusSquare, Search, TrendingUp } from 'lucide-react'
 import React from 'react'
+import { useNavigate } from "react-router-dom"
+import { toast } from "sonner"
 
 
 
 
 const LeftSideBar = () => {
 
+    const navigate = useNavigate();
 
+    const logoutHandler = async () => {
+        try {
+            const res = await axios.get('http://localhost:8000/api/v1/user/logout', {
+                withCredentials: true
+            });
+            if (res.data.success) {
+                navigate('/login')
+                toast.success(res.data.message)
+            }
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }
+    };
+    const sidebarHandler = (textType) => {
+        if(textType === "Logout") logoutHandler()
+    }
     const sideBarItems = [
         {
             icon: <Home />,
@@ -60,7 +80,9 @@ const LeftSideBar = () => {
                 <div ></div>
                 {sideBarItems.map((item, index) => {
                     return (
-                        <div key={index}
+                        <div
+                        onClick={() => sidebarHandler(item.text)}
+                        key={index}
                             className='flex items center gap-3 relative hover:bg-gray-100 cursor-pointer rounded-lg p-3'>
                             {item.icon}
                             <span>{item.text}</span>
