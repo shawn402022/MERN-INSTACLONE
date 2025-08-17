e3ac75b614faad812d9094b0a5cd4ce25f294501
Shawn <sgiovanni1@yahoo.com>
2025-08-17 01:40:21 -0400

    styled profile picture, and added edit profile and view archive buttons as well as profile name next to picture

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import useGetUserProfile from '@/hooks/useGetUserProfile'
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { Button } from '@/components/ui/button';
 
 
 
@@ -11,15 +12,35 @@ const Profile = () => {
     const userId = params.id
     useGetUserProfile(userId);
 
-    const {userProfile} = useSelector((store) => store.auth);
-    console.log("userprofile", userProfile)
+    const { userProfile } = useSelector((store) => store.auth);
 
     return (
-        <div>
-            <Avatar >
-                <AvatarImage src={userProfile?.profilePicture} alt="Profile Picture"/>
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+        <div className="flex max-w-4xl justify-center mx-auto">
+            <div className="flex flex-col gap-20 p-8">
+                <div className="grid grid-cols-2">
+                    <section className="flex items-center justify-center">
+                        <Avatar className=" h-32 w-32" >
+                            <AvatarImage src={userProfile?.profilePicture} alt="Profile Picture" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+
+                    </section>
+                    <section>
+                        <div className="flex flex-col gap-5">
+                            <div className="flex items-center gap-2">
+                                <span>{userProfile?.username}</span>
+                                <>
+                                    <Button variant="secondary" className="hover:bg-gray-200 h-8">Edit Profile
+                                    </Button>
+                                    <Button variant="secondary" className="hover:bg-gray-200 h-8">View Archive
+                                    </Button>
+                                </>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+
         </div>
     )
 }
