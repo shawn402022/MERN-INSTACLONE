ee308afa20035208491d139463f899803a735659
Shawn <sgiovanni1@yahoo.com>
2025-08-17 01:29:36 -0400

    created custom hook to fetch user profile, and called the function useGetuserproile  in the profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import useGetUserProfile from '@/hooks/useGetUserProfile'
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 
 const Profile = () => {
+    const params = useParams();
+    const userId = params.id
+    useGetUserProfile(userId);
+
+    const {userProfile} = useSelector((store) => store.auth);
+    console.log("userprofile", userProfile)
+
     return (
         <div>
             <Avatar >
-                <AvatarImage src={post.author?.profilePicture} alt='User' />
+                <AvatarImage src={userProfile?.profilePicture} alt="Profile Picture"/>
                 <AvatarFallback>CN</AvatarFallback>
             </Avatar>
         </div>
